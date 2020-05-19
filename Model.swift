c642901df535f13d51fca327134fa8e28287a355
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-19 11:54:37 +0100

    Model created

---
+++ Model.swift
@@ -0,0 +1,45 @@
+//
+//  Model.swift
+//  GitHub Search
+//
+//  Created by Emil Vaklinov on 19/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+struct GitHubRoot: Codable {
+    var items: [Items]
+}
+
+struct Items: Codable {
+    var id: Int
+    var name: String
+    var fullName: String
+    var owner: Owner
+    var htmlUrl: String
+    var description: String?
+    var language: String?
+    var forks: Int
+    var openIssues: Int
+    var watchers: Int
+    var score: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+        case htmlUrl = "html_url"
+        case openIssues = "open_issues"
+        case name,owner,description,language,forks,watchers,score,id
+    }
+}
+
+struct Owner: Codable {
+    var avatarUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+    }
+}
