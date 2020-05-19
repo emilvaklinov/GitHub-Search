2f7714907adf4e5fe7c2b2164f5124c86331a7f9
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-19 12:28:10 +0100

    Search VC and view created

---
+++ Custom Cells/SearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCell.swift
+//  GitHub Search
+//
+//  Created by Emil Vaklinov on 19/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
