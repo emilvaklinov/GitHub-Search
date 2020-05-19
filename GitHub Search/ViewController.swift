e9c2016641a7afadb1e2fef9538b12804d345b7c
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-19 11:49:16 +0100

    Initial Commit

---
+++ GitHub Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Search
+//
+//  Created by Emil Vaklinov on 19/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
