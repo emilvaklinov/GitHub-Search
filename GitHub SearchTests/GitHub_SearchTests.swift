ffd97a02284c685162aea67b492272982aa38017
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-20 13:33:56 +0100

    Bug fixing

--- GitHub SearchTests/GitHub_SearchTests.swift
+++ GitHub SearchTests/GitHub_SearchTests.swift
@@ -9,14 +9,44 @@
 import XCTest
 @testable import GitHub_Search
 
+class MockURLSession: URLSession {
+    
+    private let mockTask: MockTask
+    init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+        mockTask = MockTask(data: data, urlResponse: urlResponse, error: error)
+    }
+    
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        mockTask.completionHandler = completionHandler
+        return mockTask
+    }
+}
+
+class MockTask: URLSessionDataTask {
+    private let data: Data?
+    private let urlResponse: URLResponse?
+    private let taskError: Error?
+    
+    var completionHandler: ((Data?, URLResponse?, Error?) -> Void)?
+    init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+        self.data = data
+        self.urlResponse = urlResponse
+        self.taskError = error
+    }
+    
+    override func resume() {
+        DispatchQueue.main.async {
+            self.completionHandler?(self.data, self.urlResponse, self.taskError)
+        }
+    }
+}
+
+
 class GitHub_SearchTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    var sut = APIManager?.self
+    override func tearDown() {
+       
     }
 
     func testExample() throws {
